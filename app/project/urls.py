f4ffc2da70d7c9c34d12fc09cc51a88ab23ff3bb
Matheus Medeiros <mmmatheus013@gmail.com>
2025-02-25 18:04:16 -0300

    Removing API from URLs

--- app/project/urls.py
+++ app/project/urls.py
@@ -4,14 +4,14 @@ from views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('accounts.urls')),
-    path('api/', include('authentication.urls')),
+    path('', include('accounts.urls')),
+    path('', include('authentication.urls')),
     path('auth/', include('allauth.urls')),
-    path('api/', include('plans.urls')),
-    path('api/', include('tutorials.urls')),
-    path('api/', include('campaigns.urls')),
-    path('api/', include('integrations.urls')),
-    path('api/', include('payments.urls')),
+    path('', include('plans.urls')),
+    path('', include('tutorials.urls')),
+    path('', include('campaigns.urls')),
+    path('', include('integrations.urls')),
+    path('', include('payments.urls')),
     path('', index, name='index'),
 
 ]
