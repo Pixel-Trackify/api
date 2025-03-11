c7b590bb3919dbbe37cf282572dd3ab299c7b4b5
Matheus Medeiros <mmmatheus013@gmail.com>
2025-03-11 13:33:47 -0300

    return user in token verify

--- app/authentication/urls.py
+++ app/authentication/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
-
+from .views import CustomTokenVerifyView
 
 urlpatterns = [
     path('authentication/token/', TokenObtainPairView.as_view(),
@@ -8,5 +8,5 @@ urlpatterns = [
     path('authentication/token/refresh/',
          TokenRefreshView.as_view(), name='token_refresh'),
     path('authentication/token/verify/',
-         TokenVerifyView.as_view(), name='token_verify'),
+         CustomTokenVerifyView.as_view(), name='token_verify'),
 ]
