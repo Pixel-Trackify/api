1cd57c6f62ac45c105a58b4d63e45cd63ad4ce08
Matheus Medeiros <mmmatheus013@gmail.com>
2025-03-17 12:23:52 -0300

    fix url

--- app/tutorials/urls.py
+++ app/tutorials/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('tutoriais/', views.TutorialListView.as_view(),
          name='tutorial-list-create'),
-    path('tutorials/create/', views.TutorialCreateView.as_view(),
+    path('tutoriais/create/', views.TutorialCreateView.as_view(),
          name='tutorial-create'),
     path('tutoriais/<int:pk>/',
          views.TutorialRetrieveUpdateDestroyView.as_view(), name='tutorial-detail'),
