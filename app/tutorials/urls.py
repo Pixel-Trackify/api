a55bbe24b16c4d43246bdd25fc4f704221579d62
Matheus Medeiros <mmmatheus013@gmail.com>
2025-03-17 12:04:30 -0300

    fix: restrict tutorial creation to admin users only and thumbnail_url

--- app/tutorials/urls.py
+++ app/tutorials/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('tutoriais/', views.TutorialListCreateView.as_view(),
+    path('tutoriais/', views.TutorialListView.as_view(),
          name='tutorial-list-create'),
+    path('tutorials/create/', views.TutorialCreateView.as_view(),
+         name='tutorial-create'),
     path('tutoriais/<int:pk>/',
          views.TutorialRetrieveUpdateDestroyView.as_view(), name='tutorial-detail'),
 ]
