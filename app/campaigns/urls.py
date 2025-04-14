b33d6c7a2a3765a62eac041b333d6dd3bb0893b7
Matheus Medeiros <mmmatheus013>
2025-04-14 17:24:52 -0300

    fix:clean code

--- app/campaigns/urls.py
+++ app/campaigns/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CampaignViewSet, KwaiWebhookView
+from .views import CampaignViewSet
 
 router = DefaultRouter()
 router.register(r'campaigns', CampaignViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('webhook/kwai/<uuid:uid>/',
-         KwaiWebhookView.as_view(), name='kwai-webhook'),
+
 ]
