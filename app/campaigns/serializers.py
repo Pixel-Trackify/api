74b592efa355e229e0d879524890b8a07ddd31a5
Matheus Medeiros <mmmatheus013>
2025-04-30 08:30:11 -0300

    fix: return id

--- app/campaigns/serializers.py
+++ app/campaigns/serializers.py
@@ -28,7 +28,7 @@ class CampaignSerializer(serializers.ModelSerializer):
     class Meta:
         model = Campaign
         fields = [
-            'id', 'uid', 'integrations', 'user', 'source', 'title', 'CPM', 'CPC', 'CPV', 'method',
+            'uid', 'integrations', 'user', 'source', 'title', 'CPM', 'CPC', 'CPV', 'method',
             'total_approved', 'total_pending', 'amount_approved', 'amount_pending', 'total_abandoned', 'amount_abandoned', 'total_canceled', 'amount_canceled', 'total_refunded', 'amount_refunded', 'total_rejected', 'amount_rejected', 'total_chargeback', 'amount_chargeback',
             'total_ads', 'profit', 'ROI', 'total_views', 'total_clicks',
             'created_at', 'updated_at', 'stats', 'overviews'
