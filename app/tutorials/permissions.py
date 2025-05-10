845f54f6bef69669e3d3278c1b0df6490987ae55
Matheus Medeiros <mmmatheus013>
2025-05-10 20:10:37 -0300

    fix: refine permission checks in IsAdminUserOrReadOnly to streamline access control

--- app/tutorials/permissions.py
+++ app/tutorials/permissions.py
@@ -5,7 +5,10 @@ class IsAdminUserOrReadOnly(BasePermission):
 
     def has_permission(self, request, view):
 
-        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+        if request.method in ['GET', 'OPTIONS']:
+
+            if view.action == 'retrieve' and not request.user.is_superuser:
+                return False
             return True
 
         return request.user and request.user.is_superuser
