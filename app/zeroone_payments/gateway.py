6d21061ba9bfd4329243485c0c49e89702347db1
Matheus Medeiros <mmmatheus013>
2025-05-10 14:57:50 -0300

    fix: remove excessive logging in ZeroOneGateway to improve code clarity

--- app/zeroone_payments/gateway.py
+++ app/zeroone_payments/gateway.py
@@ -14,20 +14,13 @@ class ZeroOneGateway:
         }
 
         try:
-            logger.debug("Enviando requisição ao gateway ZeroOne...")
-            logger.debug(f"URL: {settings.ZEROONE_API_URL}")
-            logger.debug(f"Headers: {headers}")
-            logger.debug(f"Payload: {payload}")
 
             response = requests.post(
                 settings.ZEROONE_API_URL, json=payload, headers=headers)
 
-            logger.debug(f"Status Code: {response.status_code}")
-            logger.debug(f"Resposta do Gateway: {response.text}")
-
             response.raise_for_status()
             return response.json()
         except requests.exceptions.RequestException as e:
-            logger.error(f"Erro ao comunicar com o gateway ZeroOne: {str(e)}")
+
             raise Exception(
                 f"Erro ao comunicar com o gateway ZeroOne: {str(e)}")
