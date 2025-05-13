910a801794b4b7d53b89a9b58c644e54ef0eba41
Matheus Medeiros <mmmatheus013>
2025-05-13 18:33:01 -0300

    refactor: remove zeroone_payments module and related files

--- app/zeroone_payments/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class ZeroOnePaymentsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'zeroone_payments'
-
-    def ready(self):
-        import zeroone_payments.signals  
