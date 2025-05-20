a404d73cd40db4e06eb99e2697ade3cf5d04023b
Matheus Medeiros <mmmatheus013>
2025-05-19 23:15:03 -0300

    fix(goal): remove 'description' field from GoalSerializer and clean up code

--- app/goals/serializers.py
+++ app/goals/serializers.py
@@ -9,7 +9,7 @@ class GoalSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Goal
-        fields = ['uid', 'prize', 'description', 'image',
+        fields = ['uid', 'prize', 'image',
                   'min', 'max', 'created_at', 'updated_at']
 
     def validate(self, data):
@@ -19,18 +19,16 @@ class GoalSerializer(serializers.ModelSerializer):
         min_value = data.get('min', 0)
         max_value = data.get('max', 0)
 
-        
         if min_value < 0 or max_value <= 0:
             raise serializers.ValidationError(
                 "Os valores 'min' e 'max' devem ser maiores que 0.")
 
-        
         if max_value < min_value:
             raise serializers.ValidationError(
                 "O valor 'max' não pode ser menor que o valor 'min'.")
 
         # Validação: range de valores já cadastrado
-        instance = self.instance  
+        instance = self.instance
         query = Q(min__lte=min_value, max__gte=min_value) | Q(
             min__lte=max_value, max__gte=max_value)
         if instance:
@@ -56,10 +54,10 @@ class GoalSerializer(serializers.ModelSerializer):
         Sobrescreve o método create para fazer o upload da imagem ao S3.
         """
         image = validated_data.pop('image', None)
-        user = self.context['request'].user  
+        user = self.context['request'].user
         if image:
             validated_data['image'] = upload_to_s3(
-                image, user)  
+                image, user)
         return super().create(validated_data)
 
     def update(self, instance, validated_data):
@@ -67,8 +65,8 @@ class GoalSerializer(serializers.ModelSerializer):
         Sobrescreve o método update para fazer o upload da imagem ao S3.
         """
         image = validated_data.pop('image', None)
-        user = self.context['request'].user  
+        user = self.context['request'].user
         if image:
             validated_data['image'] = upload_to_s3(
-                image, user)  
+                image, user)
         return super().update(instance, validated_data)
