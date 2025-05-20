44eec303d8df2a1d2520566d06ddf0b9da057eac
Matheus Medeiros <mmmatheus013>
2025-05-20 19:18:37 -0300

    feat(campaigns): add webhook URLs to campaign detail response

--- app/campaigns/views.py
+++ app/campaigns/views.py
@@ -133,6 +133,26 @@ class CampaignViewSet(viewsets.ModelViewSet):
 
         return response
 
+    def retrieve(self, request, *args, **kwargs):
+        """Adiciona os links do webhook na resposta do detalhe da campanha"""
+        response = super().retrieve(request, *args, **kwargs)
+
+        # Recuperar a campanha detalhada
+        campaign = Campaign.objects.get(uid=response.data['uid'])
+
+        # Usar o domínio configurado no .env
+        base_webhook_url = settings.WEBHOOK_BASE_URL
+
+        # Construir as URLs do webhook
+        view_webhook_url = f"{base_webhook_url}{campaign.uid}/?action=view"
+        click_webhook_url = f"{base_webhook_url}{campaign.uid}/?action=click"
+
+        # Adicionar as URLs do webhook na resposta
+        response.data['view_webhook_url'] = view_webhook_url
+        response.data['click_webhook_url'] = click_webhook_url
+
+        return response
+
     def destroy(self, request, *args, **kwargs):
         """
         Permite deletar uma única campanha pela `uid` na URL.
