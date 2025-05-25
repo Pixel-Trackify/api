c89047dde0a9a689e0828d140d3c8fe07ba20947
Matheus Medeiros <mmmatheus013>
2025-05-25 11:14:09 -0300

    fix(serializers): update CaptchaSerializer fields to remove unused recaptcha keys

--- app/custom_admin/serializers.py
+++ app/custom_admin/serializers.py
@@ -13,8 +13,8 @@ class ConfigurationSerializer(serializers.ModelSerializer):
 class CaptchaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Configuration
-        fields = ['recaptchar_enable',
-                  'recaptchar_site_key', 'recaptchar_secret_key']
+        fields = [
+            'recaptchar_site_key']
 
 
 class UsuarioSerializer(serializers.ModelSerializer):
